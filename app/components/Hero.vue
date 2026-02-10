b08c11d9e4be5f74d441771d070135ac24c7c580
mccarthyk <mccarthyk@hcflgov.net>
2026-02-10 13:44:49 -0500

    hero spacing

--- app/components/Hero.vue
+++ app/components/Hero.vue
@@ -1,7 +1,5 @@
 <template>
-  <section
-    class="relative w-full h-full overflow-hidden"
-  >
+  <section class="relative w-full h-full overflow-hidden">
     <div class="object-cover absolute inset-0">
       <Carousel />
 
@@ -13,19 +11,15 @@
         variant="solid"
         class="flex flex-col items-center bg-transparent text-shadow-lg"
       >
-        <template #header>
-          <img src="/images/brand/recycleLogo.png" alt="" class="mx-auto" />
-        </template>
-
-        <div class="[&_h1]:text-6xl [&_h1]:font-bold [&_h1]:text-sky-400 text-center">
+        <img src="/images/brand/recycleLogo.png" alt="" class="mx-auto" />
+        <div
+          class="[&_h1]:text-6xl [&_h1]:font-bold [&_h1]:text-sky-400 text-center"
+        >
           <slot name="title" />
         </div>
-
-        <template #footer>
-          <div class="text-2xl text-center">
-            <slot name="description" />
-          </div>
-        </template>
+        <div class="text-2xl text-center">
+          <slot name="description" />
+        </div>
       </UCard>
     </div>
   </section>
