c6b4351adff71ab7d7a938daec35cc6948bf6610
Jordan Pisani <pisanij@hillsboroughcounty.org>
2026-02-27 11:26:27 -0500

    increase tab specificity, add important

--- app/app.vue
+++ app/app.vue
@@ -29,4 +29,15 @@
   outline: 3px solid #0a84ff;
   outline-offset: 2px;
 }
+
+[role="tablist"]:focus-visible,
+[role="tablist"]:focus {
+  outline: 3px solid #0a84ff !important;
+  outline-offset: 3px !important;
+}
+[role="tab"]:focus-visible,
+[role="tab"]:focus {
+  outline: 3px solid #0a84ff !important;
+  outline-offset: 3px !important;
+}
 </style>
